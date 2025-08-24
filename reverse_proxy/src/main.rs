0c3f4a4f32b8e201a151d1f6d0326a251fd25ff5
Renan <machadosantosrenan@gmail.com>
2025-08-24 13:54:55 -0300

    Made basic host validation system!

--- reverse_proxy/src/main.rs
+++ reverse_proxy/src/main.rs
@@ -3,19 +3,25 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::thread;
+use std::sync::{Arc, Mutex};
+
+type SharedSecret = Arc<Mutex<Option<String>>>;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
 
+    let secret_state: SharedSecret = Arc::new(Mutex::new(None));
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        let secret_state_clone = Arc::clone(&secret_state);
         thread::spawn(move ||{
-            proxy_foward(stream);
+            proxy_foward(stream, secret_state_clone);
         });
     }
 }
 
-fn proxy_foward(mut stream: TcpStream) {
+fn proxy_foward(mut stream: TcpStream, secret_state: SharedSecret) {
     let mut buffer = [0; 4096];
     stream.read(&mut buffer).unwrap();
     let request_string = String::from_utf8_lossy(&buffer[..]);
@@ -23,38 +29,60 @@ fn proxy_foward(mut stream: TcpStream) {
         "Request: {}", String::from_utf8_lossy(&buffer[..])
     );*/
     let mut method = "N/A".to_string();
-    let mut host = "N/A".to_string();
+    let mut path = "N/A".to_string();
 
-    for i in 0..2 {
-    
-        if i == 0 {
-            method = request_string.lines().next().unwrap().to_string();
-        } else if i == 1 {
-            host = request_string.lines().skip(1).next().unwrap().to_string();
-        }
-    }
+    let main_header = request_string.lines().next().unwrap().to_string();
+    let mut parts = main_header.split_whitespace();
+    method = parts.next().unwrap().to_string();
+    path = parts.next().unwrap().to_string();
 
-    if method.starts_with("GET") {
-        println!("[REVERSE PROXY]: Recebemos uma requisição do tipo: GET");
 
-        let mut server_stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-        let server_req = format!(
-            "{}\r\nHost:{}\r\nKey:revproxy\r\n\r\n", method, host
-        );
-        server_stream.write_all(server_req.as_bytes()).unwrap();
-        server_stream.flush().unwrap();
+    if method == "POST" && path == "/register-secret" {
 
-        println!("[REVERSE PROXY]: Fowarded requisition!");
-        let mut response_buffer = [0; 4096];
-        server_stream.read(&mut response_buffer).unwrap();
-        println!(
-            "Response: {}", String::from_utf8_lossy(&response_buffer[..])
-        );
-        stream.write(&response_buffer).unwrap();
-        stream.flush().unwrap();
-        
+        let body = request_string.split("\r\n\r\n").nth(1).unwrap_or("").trim_end_matches('\0');
+
+        let mut secret_guard = secret_state.lock().unwrap();
+        *secret_guard = Some(body.to_string());
+
+        println!("[REVERSE PROXY]: Received server's key: {:?}", *secret_guard);
+
+        let response = "HTTP/1.1 200 OK \r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
 
     } else {
-        println!("[REVERSE PROXY]: Recebemos uma nova requisição => \n{}", request_string);
-    }
+        let secred_guard = secret_state.lock().unwrap();
+        let secret = match &*secred_guard {
+            Some(s) => s.clone(),
+            None => {
+                let response = "HTTP/1.1 503 Service Unavaible\r\n\r\nProxy not ready";
+                stream.write(response.as_bytes()).unwrap();
+                return;
+            }
+        };
+        drop(secred_guard);
+
+        if method.starts_with("GET") {
+            println!("[REVERSE PROXY]: Recebemos uma requisição do tipo: GET");
+
+            let mut server_stream = TcpStream::connect("127.0.0.1:7878").unwrap();
+            let server_req = format!(
+                "X-Proxy-Signature: {}\r\n{} {}\r\nHost: 127.0.0.1:7878\r\n\r\n",secret, method, path
+            );
+            server_stream.write_all(server_req.as_bytes()).unwrap();
+            server_stream.flush().unwrap();
+
+            println!("[REVERSE PROXY]: Fowarded requisition!");
+            let mut response_buffer = [0; 4096];
+            server_stream.read(&mut response_buffer).unwrap();
+            println!(
+                "Response: {}", String::from_utf8_lossy(&response_buffer[..])
+            );
+            stream.write(&response_buffer).unwrap();
+            stream.flush().unwrap();
+            
+
+        } else {
+            println!("[REVERSE PROXY]: Recebemos uma nova requisição não-roteada => \n{}", request_string);
+        }
+    } 
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,16 @@
+use std::fmt::format;
 use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::thread;
 use std::path::Path;
+use rand::Rng;
+use sha2::{Sha256, Digest};
+use std::sync::Arc;
 
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, secret: Arc<String>) {
     let mut buffer = [0; 4096];
     stream.read(&mut buffer).unwrap();
     
@@ -18,6 +22,11 @@ fn handle_connection(mut stream: TcpStream) {
     let mut key = "N/A".to_string();
 
     if let Some(request_line) = request_string.lines().next() {
+        let (_, proxy_secret) = request_line.split_once(": ").unwrap();
+        key = proxy_secret.to_string();
+    }
+
+    if let Some(request_line) = request_string.lines().skip(1).next() {
         let mut parts = request_line.split_whitespace();
         if let Some(l) = parts.next(){
             method = l.to_string();
@@ -37,20 +46,21 @@ fn handle_connection(mut stream: TcpStream) {
 
             match header_name.to_lowercase().as_str(){
                 "host" => host = header_value.to_string(),
-                "key" => key = header_value.to_string(),
                 _ => {}
             }
         }
     }
 
-    println!("[SERVER]: Recebemos um novo request =>\nMétodo: {}\nValor: {}\nHost: {}\nAccess-Key: {}", method, value, host, key);
+    println!("[SERVER]: Recebemos um novo request =>\nMétodo:{}\nValor:{}\nHost:{}\nAccess-Key:{}", method, value, host, key);
 
     /*println!(
         "Request: {}", String::from_utf8_lossy(&buffer[..])
     );*/
 
     //let get = b"GET / HTTP/1.1\r\n";
-    if key == "revproxy" {
+    dbg!(&secret);
+    dbg!(&key);
+    if key == secret.as_str() {
         if method == "GET" {
             let (_, req_content) = value.split_once("/").unwrap();
             //println!("[SERVER] : Recebemos uma requisição GET!");
@@ -81,14 +91,27 @@ fn handle_connection(mut stream: TcpStream) {
                 
             }
 
-            let contents = fs::read_to_string(filepath).unwrap();
+            let response;
+            if filepath.exists() {
+                let contents = fs::read_to_string(filepath).unwrap();
 
-            let response = format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
-                content_type,
-                contents.len(),
-                contents
-            );
+                response = format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                    content_type,
+                    contents.len(),
+                    contents
+                );
+            } else {
+                let contents = fs::read_to_string("404.html").unwrap();
+
+                response = format!(
+                    "HTTP/1.1 404 NOT FOUND\r\nContent-Lengh: {}\r\n\r\n{}",
+                    contents.len(),
+                    contents
+                );
+            }
+
+            
 
             stream.write(response.as_bytes()).unwrap();
             stream.flush().unwrap();
@@ -128,15 +151,76 @@ fn handle_connection(mut stream: TcpStream) {
     // message-body
 }
 fn main() {
+    const CHARSET_SENHA_FORTE: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                                        abcdefghijklmnopqrstuvwxyz\
+                                        0123456789\
+                                        !@#$%^&*()_+-=[]{}|;:,.<>?";
+
+    let secret_string = gerar_secret_string(16, CHARSET_SENHA_FORTE);
+
+    let mut hasher = Sha256::new();
+    hasher.update(secret_string.as_bytes());
+    let hash_byted = hasher.finalize();
+    let secret_key = hex::encode(hash_byted);
+
+    println!("[SERVER]: Generated secret_key: {}", secret_key);
+
+    if let Err(e) = register_with_proxy(&secret_key.as_str()) {
+        eprintln!("[SERVER]: Critical Error: {}", e);
+    }
+
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let secret_key_arc = Arc::new(secret_key);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        
+        let secret_clone = Arc::clone(&secret_key_arc);
         thread::spawn(move || {
-            handle_connection(stream);
+            handle_connection(stream, secret_clone);
         });
         
 
     }
+}
+
+fn gerar_secret_string(tamanho: usize, charset: &[u8]) -> String {
+    let mut rng = rand::rng();
+    (0..tamanho).map(|_| {
+        let indice = rng.random_range(0..charset.len());
+        charset[indice] as char
+    }).collect()
+}
+
+fn register_with_proxy(secret: &str) -> Result<(), String> {
+    println!("[SERVER]: Trying to register secret_key!");
+    match TcpStream::connect("0.0.0.0:3000") {
+        Ok(mut stream) => {
+            let request = format!(
+                "POST /register-secret HTTP/1.1\r\n\
+                Host: 127.0.0.1:7878\r\n\
+                Content-Type: text/plain\r\n\
+                Content-Length: {}\r\n\
+                \r\n\
+                {}",
+                secret.len(),
+                secret
+            );
+
+            stream.write(request.as_bytes()).unwrap();
+
+            let mut response_buffer = [0; 512];
+            stream.read(&mut response_buffer).map_err(|e| e.to_string())?;
+            let response_str = String::from_utf8_lossy(&response_buffer);
+
+            if response_str.starts_with("HTTP/1.1 200 OK") {
+                println!("[SERVER]: Secret-key has been setted up!");
+                Ok(())
+            } else {
+                Err(format!("Erro no registro. Resposta do Proxy:\n{}", response_str))
+            }
+        }
+        Err(e) => Err(format!("Falha ao se conectar com o Proxy: {}", e))
+    }
+
+    
 }
