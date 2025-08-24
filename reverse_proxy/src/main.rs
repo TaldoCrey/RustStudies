cd142897aaa463ac1e9df6470700e73bc8357620
Renan <machadosantosrenan@gmail.com>
2025-08-24 10:46:18 -0300

    Made some improvements in server and reverse proxy parsing!

--- reverse_proxy/src/main.rs
+++ reverse_proxy/src/main.rs
@@ -2,32 +2,46 @@ use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::thread;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        proxy_foward(stream);
+        thread::spawn(move ||{
+            proxy_foward(stream);
+        });
     }
 }
 
 fn proxy_foward(mut stream: TcpStream) {
     let mut buffer = [0; 4096];
     stream.read(&mut buffer).unwrap();
-
-    println!(
+    let request_string = String::from_utf8_lossy(&buffer[..]);
+    /*println!(
         "Request: {}", String::from_utf8_lossy(&buffer[..])
-    );
+    );*/
+    let mut method = "N/A".to_string();
+    let mut host = "N/A".to_string();
 
-    let get = b"GET /";
+    for i in 0..2 {
+    
+        if i == 0 {
+            method = request_string.lines().next().unwrap().to_string();
+        } else if i == 1 {
+            host = request_string.lines().skip(1).next().unwrap().to_string();
+        }
+    }
 
-    if buffer.starts_with(get) {
+    if method.starts_with("GET") {
         println!("[REVERSE PROXY]: Recebemos uma requisição do tipo: GET");
 
         let mut server_stream = TcpStream::connect("127.0.0.1:7878").unwrap();
-        server_stream.write_all(&buffer).unwrap();
+        let server_req = format!(
+            "{}\r\nHost:{}\r\nKey:revproxy\r\n\r\n", method, host
+        );
+        server_stream.write_all(server_req.as_bytes()).unwrap();
         server_stream.flush().unwrap();
 
         println!("[REVERSE PROXY]: Fowarded requisition!");
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,35 +2,85 @@ use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::thread;
 
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; 4096];
     stream.read(&mut buffer).unwrap();
-    println!(
+    
+    let request_string = String::from_utf8_lossy(&buffer[..]);
+
+    let mut method = "N/A".to_string();
+    let mut value = "N/A".to_string();
+    let mut host = "N/A".to_string();
+    let mut key = "N/A".to_string();
+
+    if let Some(request_line) = request_string.lines().next() {
+        let mut parts = request_line.split_whitespace();
+        if let Some(l) = parts.next(){
+            method = l.to_string();
+        }
+
+        if let Some(m) = parts.next() {
+            value = m.to_string();
+        }
+    }
+
+    for line in request_string.lines().skip(1) {
+        if line.is_empty() {
+            break;
+        }
+        //println!("line = {}", line);
+        if let Some((header_name, header_value)) = line.split_once(":") {
+
+            match header_name.to_lowercase().as_str(){
+                "host" => host = header_value.to_string(),
+                "key" => key = header_value.to_string(),
+                _ => {}
+            }
+        }
+    }
+
+    println!("[SERVER]: Recebemos um novo request =>\nMétodo: {}\nValor: {}\nHost: {}\nAccess-Key: {}", method, value, host, key);
+
+    /*println!(
         "Request: {}", String::from_utf8_lossy(&buffer[..])
-    );
+    );*/
 
-    let get = b"GET / HTTP/1.1\r\n";
-        
-    if buffer.starts_with(get) {
-        println!("[SERVER] : Recebemos uma requisição GET!");
+    //let get = b"GET / HTTP/1.1\r\n";
+    if key == "revproxy" {
+        if method == "GET" {
+            //println!("[SERVER] : Recebemos uma requisição GET!");
 
-        let contents = fs::read_to_string("index.html").unwrap();
+            let contents = fs::read_to_string("index.html").unwrap();
 
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Lengh: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
+            let response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Lengh: {}\r\n\r\n{}",
+                contents.len(),
+                contents
+            );
 
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        } else {
+            let contents = fs::read_to_string("404.html").unwrap();
+
+            let response = format!(
+                "HTTP/1.1 404 NOT FOUND\r\nContent-Lengh: {}\r\n\r\n{}",
+                contents.len(),
+                contents
+            );
+
+            stream.write(response.as_bytes()).unwrap();
+            stream.flush().unwrap();
+        }
+
     } else {
-        let contents = fs::read_to_string("404.html").unwrap();
+        let contents = fs::read_to_string("403.html").unwrap();
 
         let response = format!(
-            "HTTP/1.1 404 NOT FOUND\r\nContent-Lengh: {}\r\n\r\n{}",
+            "HTTP/1.1 403 FORBIDDEN\r\nContent-Lengh: {}\r\n\r\n{}",
             contents.len(),
             contents
         );
@@ -38,6 +88,8 @@ fn handle_connection(mut stream: TcpStream) {
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
+        
+        
 
     
 
@@ -51,8 +103,11 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connection(stream);
+        
+        thread::spawn(move || {
+            handle_connection(stream);
+        });
+        
 
     }
 }
